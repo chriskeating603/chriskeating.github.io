495df629e8da59e2034d7ea3c958d007a25602af
Chris Keating <ckeating.nh@gmail.com>
2024-05-07 23:38:25 -0700

    Add loneliness article and baba demo + update styling a lil

--- src/components/WritingsProjects.tsx
+++ src/components/WritingsProjects.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Box, Card, CardActionArea, Typography, Grid, CardMedia, Container } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import GitHubLogo from '../assets/githubLogo.svg'; // Adjust the path as necessary
-import PenPaperLogo from '../assets/penPaperLogo.svg'; // Adjust the path as necessary
+// import GitHubLogo from '../assets/githubLogo.svg'; // Adjust the path as necessary
+// // src/assets/githubLogo.svg
+// import PenPaperLogo from '../assets/penPaperLogo.svg'; // Adjust the path as necessary
+// require('../assets/images/senior_alone_square.jpeg') ;
 
 // Assuming Article type with added image property
 type Article = {
@@ -16,25 +18,34 @@ type Article = {
 const articles: Article[] = [
   {
     id: '1',
-    title: 'Elder Companion',
+    title: '[Article] Why Senior Loneliness Matters',
+    subtitle: 'It’s a large problem in US, for many reasons — but it is a problem we can solve, together',
+    link: 'https://chriskeating603.medium.com/why-senior-loneliness-matters-154b94343465',
+    imageUrl: 'https://ckeatingnh-images.s3.us-east-2.amazonaws.com/senior_alone_square.jpeg',
+  },
+  
+  {
+    id: '2',
+    title: '[Project] Elder Companion',
     subtitle: 'AI-powered companion to combat loneliness in elderly folks - built in 1 day',
     link: 'https://github.com/chriskeating603/elder-companion',
     imageUrl: 'https://github.com/chriskeating603/elder-companion/blob/main/src/assets/images/logo.png?raw=true',
   },
   {
-    id: '2',
-    title: 'Judgement Day',
+    id: '3',
+    title: '[Demo] Baba\'s App',
+    subtitle: 'An app for my grandmother to check the weather in her family\'s location and check out photos in a shared photo album',
+    link: 'https://www.loom.com/share/4c6526d651444db5b0cb967aec8fa451',
+    imageUrl: 'https://ckeatingnh-images.s3.us-east-2.amazonaws.com/baba_demo.jpg',
+  },
+  {
+    id: '4',
+    title: '[Project] Judgement Day',
     subtitle: 'Hackathon project to judge other hackathon projects. Meta-hack, if you will. Got second place at AI Startup Hackathon by SiliconValleyDiary.com',
     link: 'https://github.com/chriskeating603/judgement-day',
     imageUrl: 'https://github.com/chriskeating603/judgement-day/blob/main/web/public/one.png?raw=true',
   },
-  {
-    id: '3',
-    title: 'Grammy Predictions',
-    subtitle: 'Predicting the grammy award winners based on song popularity, musical attributes, and historical trends',
-    link: 'https://github.com/chriskeating603/grammy-predictions',
-    imageUrl: 'https://github.com/chriskeating603/chriskeating.github.io/blob/second_iteration/assets/img/podcasts.png?raw=true',
-  },
+
   // {
   //   id: '4',
   //   title: 'Article Title 4',
@@ -51,6 +62,7 @@ const HoverCard = styled(Card)(({ theme }) => ({
   '&:hover': {
     '& .overlay': {
       opacity: 1,
+      padding: '5px',
     },
   },
 }));
@@ -106,7 +118,7 @@ const Personal = () => {
                     alt={article.title}
                     style={{ height: 300, objectFit: 'cover' }}
                   />
-                  <Logo src={article.link.includes('github.com') ? GitHubLogo : PenPaperLogo} alt="Logo" />
+                  {/* <Logo src={article.link.includes('github.com') ? GitHubLogo : PenPaperLogo} alt="Logo" /> */}
                   <Overlay className="overlay">
                     <Typography variant="h6">{article.title}</Typography>
                     <Typography variant="body2">{article.subtitle}</Typography>
